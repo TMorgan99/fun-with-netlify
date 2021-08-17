084dea414d5cd758bf027446e84343775f73a357
Tim Morgan <TMorgan99@GMail.com>
2021-08-17 11:40:19 -0700

    netlify deploy

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
@@ -8,22 +8,12 @@
   <h1>Hello world!</h1>
 
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+      "Helvetica Neue", sans-serif;
   }
 
   main {
@@ -47,19 +37,11 @@
     max-width: 14rem;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
 
   @media (min-width: 480px) {
     h1 {
       max-width: none;
     }
 
-    p {
-      max-width: none;
-    }
   }
 </style>
